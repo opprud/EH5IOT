ad7ded95683645fda6d23c97b16b16c0946c8a26
opprud <opprud@gmail.com>
2022-07-20 14:00:13 +0200

    cleanup

--- workspace/1_Hello_World/target/4.0.0-alpha.1/argon/obj/src/user_module.o.d
+++
@@ -1,6 +0,0 @@
-/Users/au263437/workspace/hih/E5IoT/workspace/1_Hello_World/target/4.0.0-alpha.1/argon//obj/./src/user_module.o: \
- src/user_module.c \
- ../../../modules/shared/nRF52840/inc/user-part/user_module.inc \
- ../../../dynalib/inc/dynalib.h
-../../../modules/shared/nRF52840/inc/user-part/user_module.inc:
-../../../dynalib/inc/dynalib.h:
